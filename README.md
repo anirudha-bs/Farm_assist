e34ac0bfaede6cd7d1a92b5936ae6ab543f36cf3
Your Name <Anirudha BS>
2020-04-20 16:58:24 +0530

    Updated files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Farm_assist
 
-A Web-app to display weather forcast and agriculture based news and to predict types of crops that can be grown in a particular soil in consideration of weather conditions using open weather api.
+A farmers assistant which predicts types of crops that could be grown in a particular soil using soil image classification and predictive analysis and also displays optimum conditions required by the crops to grow. 
+It also displays all the necessary information like weather forecast, agriculture related news, daily prices of agricultural commodities accros different markets using weather and government api's. 
+
--- application.py
+++ application.py
@@ -8,9 +8,9 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import *
 
 
-engine = create_engine("postgresql://postgres:abs%40aspire@localhost/testdb")
+engine = create_engine("DATABASE_URL")
 db = scoped_session(sessionmaker(bind=engine))
-UPLOAD_FOLDER = '/home/anirudha/Projects/Farm_assist/'
+UPLOAD_FOLDER = 'UPLOAD_FOLDER'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 name = None
 min_temp=int()
@@ -20,24 +20,24 @@ city = ""
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET','POST'])  
-def upload():  
-    return render_template('index.html') 
- 
+@app.route('/', methods = ['GET','POST'])
+def upload():
+    return render_template('index.html')
+
 @app.route("/red")
 def red():
     return render_template("upload_file.html")
 
-@app.route('/success', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
-        f = request.files['file']  
+@app.route('/success', methods = ['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
         global city
         city=request.form.get("city")
         f.save(f.filename)
         global name
         name = UPLOAD_FOLDER + f.filename
-        return redirect(url_for('predict'))  
+        return redirect(url_for('predict'))
 
 @app.route('/predict', methods=['GET','POST'])
 def predict():
@@ -46,22 +46,24 @@ def predict():
     from keras.preprocessing import image
     import h5py
 
-    model = tf.keras.models.load_model('soil_classify.h5')
-    
+    model = tf.keras.models.load_model('soil_classifier.h5')
+
     global name
     image_path = name
     img = image.load_img(image_path, target_size=(128, 128))
     plt.imshow(img)
     img = np.expand_dims(img, axis=0)
     result = model.predict_classes(img)
-    if result[0][0]:
+    if result[0]== 2:
         soil = "Red soil"
-    else:
+    elif result[0]== 1:
         soil = "Black soil"
+    else:
+        soil="Alluvial soil"
     plt.title(soil)
     path='static/predicted.jpeg'
     plt.savefig(path)
-    
+
     global city
 
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=271d1234d3f497eed5b1d80a07b3fcd1'
@@ -84,11 +86,10 @@ def predict():
 
     weather_data.append(weather)
 
-    crop_types = db.execute("select crop from soildb where soil_type = :id1 and temp_min <= :id2 and temp_max >= :id3",{"id1": soil, "id2": min_temp, "id3": max_temp}).fetchall()
+    crop_types = db.execute("select crop,temp_min,temp_max,rainfall from soildb where soil_type = :id1 and temp_min <= :id2 and temp_max >= :id3",{"id1": soil, "id2": min_temp, "id3": max_temp}).fetchall()
 
 
     return render_template('prediction.html',soil=soil,crop_types=crop_types,predicted_path = path,weather_data=weather_data,city=city)
-  
-if __name__ == '__main__':  
-    app.run(debug = True)  
 
+if __name__ == '__main__':
+    app.run(debug = True)
